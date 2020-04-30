17b5f50370ce1cddc71ebd9e67cef2cf52be00f5
Rodrigo Werberich <rodrigowerberich@hotmail.com>
2020-04-30 23:41:08 +0200

    Creating board concept

---
+++ tst/Board/Board_tester.cc
@@ -0,0 +1,32 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <array>
+#include <vector>
+
+#include "Board.hh"
+
+int main(){
+    auto result = true;
+    
+    Chess::Board::Board board;
+    
+    Chess::Board::PieceDescription pieceDescription = {
+        Chess::Board::PieceType::Pawn,
+        Chess::Board::Side::Black,
+        Chess::Board::PieceSelector::Pawn::A
+    };
+    Chess::Board::Position newPosition = 
+    {
+        Chess::Board::Column::A,
+        4
+    };
+    
+    Chess::Board::MoveResult moveResult = board.move(pieceDescription, newPosition);
+    
+    Chess::Board::Position currentPosition = board.getPosition(pieceDescription);
+    return result;
+    
+    board.undo();
+
+}
